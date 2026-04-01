d3390a437de3b28b6b8738bccc3ef8edf5006ea5
2260999-arch <2260999@flourbluffschools.org>
2026-04-01 13:09:08 -0500

    Inventory Additions
    Fixed a bug where the Y velocity would stay at a very small amount while jumping and pressing I to access the inventory.
    
    Changed colors of inventory buttons.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -27,7 +27,7 @@ func _physics_process(delta: float) -> void:
 		var direction := Input.get_axis("Left", "Right")
 		if direction == -1.0 or direction == 1.0:
 			PlayerInfo.direction = direction
-		# Handle jump.a
+		# Handle jump.
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		
@@ -46,6 +46,8 @@ func _physics_process(delta: float) -> void:
 		else:
 			if !jumping:
 				velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.y = 0
 	move_and_slide()
 
 func _process(delta: float) -> void:
