5119cdf50e50e00ec2bdb6aa9022ee7851077e07
2260999-arch <2260999@flourbluffschools.org>
2026-04-27 12:43:35 -0500

    Loot Gathering Start
    Added Wooden Chest, gives gear from the "Medieval" period.
    Plan to add three to four more chests for items from other eras, specific categories of loot, and boosters.
    Started work on loot tables for different gear.

---
+++ Scripts/loot_tables.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Used to provide loot tables for chests and other interactables that give items.
+
+#Medival Gear Table
+var medival_table = [Weapons.basic_knife, Weapons.short_sword, Weapons.long_bow, Weapons.short_bow,
+					 Armors.iron_helmet, Armors.chainmail_tunic, Armors.chainmail_pants,
+					 Armors.steel_helmet, Armors.steel_chestplate, Armors.steel_leg_harness]
+
+#Modern Gear Table
+var modern_table = [Weapons.combat_knife, Weapons.rifle, Weapons.handcannon, Weapons.pistol,
+					 Armors.combat_helmet, Armors.knee_pads, Armors.ballistic_vest,
+					 Armors.advanced_combat_helmet, Armors.heavy_ballistic_vest, Armors.armored_knee_pads]
